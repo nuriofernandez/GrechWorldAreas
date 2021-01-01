dae55254045f41ba22fddd0eee648c1c11c66014
Nurio Fernández <personal@nurio.me>
2021-01-01 20:53:07 +0100

    Initial commit

---
+++ src/main/java/me/nurio/minecraft/grechportals/GrechPortals.java
@@ -0,0 +1,17 @@
+package me.nurio.minecraft.grechportals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GrechPortals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
