3006f34040e11d9a5bf332be79062cc22c0bb226
Nurio Fernández <personal@nurio.me>
2021-01-21 19:11:11 +0100

    Make worldAreaFactory restart onEnable and removed testing event

--- src/main/java/me/nurio/minecraft/worldareas/GrechAreas.java
+++ src/main/java/me/nurio/minecraft/worldareas/GrechAreas.java
@@ -3,7 +3,6 @@ package me.nurio.minecraft.worldareas;
 import lombok.Getter;
 import me.nurio.minecraft.worldareas.configuration.ConfigLoader;
 import me.nurio.minecraft.worldareas.listeners.PlayerMovementListener;
-import me.nurio.minecraft.worldareas.listeners.PlayerAreaMovementListener;
 import me.nurio.minecraft.worldareas.areas.WorldAreaFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
@@ -11,7 +10,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GrechAreas extends JavaPlugin {
 
-    @Getter private static WorldAreaFactory worldAreaFactory = new WorldAreaFactory();
+    @Getter private static WorldAreaFactory worldAreaFactory;
     @Getter private static Plugin plugin;
 
     @Override
@@ -23,11 +22,8 @@ public class GrechAreas extends JavaPlugin {
 
         // Register events
         Bukkit.getPluginManager().registerEvents(new PlayerMovementListener(), this);
-        Bukkit.getPluginManager().registerEvents(new PlayerAreaMovementListener(), this);
-    }
 
-    @Override
-    public void onDisable() {
+        // Register area factory.
         worldAreaFactory = new WorldAreaFactory();
     }
 
