dc8ffb992d8ba7560f34d725e6ee15cbe17a806f
Nurio Fernández <personal@nurio.me>
2021-01-23 15:41:38 +0100

    Added WorldArea#getBlocks() to list all blocks inside world area

--- src/main/java/me/nurio/minecraft/worldareas/areas/WorldArea.java
+++ src/main/java/me/nurio/minecraft/worldareas/areas/WorldArea.java
@@ -2,9 +2,11 @@ package me.nurio.minecraft.worldareas.areas;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import me.nurio.minecraft.worldareas.utils.AreaUtils;
 import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -16,6 +18,8 @@ public class WorldArea {
     @Getter @NotNull private String name;
     @Getter @NotNull private UUID uuid;
     @Getter @NotNull private BlockArea area;
+
+    @Getter(lazy = true) private final List<Location> blocks = AreaUtils.getBlocksBetween(area.getStart(), area.getEnd());
 
     /**
      * Checks if the provided locations is inside the world area blocks.
