027c9f3cf6dd437e4ee87e860933e649d6b67056
xXNurioXx <personal@nurio.me>
2023-04-09 02:20:58 +0200

    Updated WorldAreaBarrier to 1.19

--- src/main/java/me/nurio/minecraft/worldareas/areas/expands/WorldAreaBarrier.java
+++ src/main/java/me/nurio/minecraft/worldareas/areas/expands/WorldAreaBarrier.java
@@ -1,6 +1,8 @@
 package me.nurio.minecraft.worldareas.areas.expands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 
@@ -13,7 +15,7 @@ public interface WorldAreaBarrier {
     default void showBarrierTo(Player player, int limit) {
         getBlocks().stream()
             .filter(location -> player.getLocation().distance(location) <= limit) // max area to render.
-            .forEach(location -> player.spawnParticle(Particle.BARRIER, location, 0));
+            .forEach(location -> player.spawnParticle(Particle.BLOCK_MARKER, location, 1, Bukkit.createBlockData(Material.BARRIER)));
     }
 
     default void showBarrierTo(Player player) {
